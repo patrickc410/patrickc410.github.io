a1fe8a3db84ccba7ea155de8e03c39f193c0d5d7
Patrick Crawford <pac410@gmail.com>
2022-08-15 16:34:50 -0400

    update resume to public/ dir; update app to provide link to resume

---
+++ src/views/Resume.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <div>
+            Hello it's me
+        </div>
+        <a href="./Patrick_resume_2022-07-19.pdf">
+            Resume Link
+        </a>
+    </div>
+</template>
