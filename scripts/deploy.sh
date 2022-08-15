2671e88b5a86cbbe6434a7dd4dd13ec4c1d58009
Patrick Crawford <pac410@gmail.com>
2022-08-15 16:10:10 -0400

    deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e 
+
+npm run build 
+
+cd dist 
+
+git init
+git add -A
+git commit -m "New Deployment"
+git push -f git@github.com:patrickc410/patrickc410.github.io.git master:gh-pages
+
+cd -
